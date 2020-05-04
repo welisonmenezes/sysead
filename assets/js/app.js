411f08e6f98bde36b081fd121219f14b9e999332
welisonmenezes <welisonmenezes@hotmail.com>
2020-05-04 14:08:42 -0300

    Implement UX enhancements to Disciplinas flow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // dropdowns
     var elems = document.querySelectorAll('.dropdown-trigger');
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidebar
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
-    
+
     //modal
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, {});
@@ -15,5 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
     // selects
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, {});
+
+    // chips
+    var elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(elems, {
+        autocompleteOptions: {
+            data: {
+                'Luciano Coelho': null,
+                'Sabrina Bet': null,
+                'Fogaça': null
+            },
+            limit: Infinity,
+            minLength: 1
+        },
+        placeholder: 'Add Professor',
+        secondaryPlaceholder: '+ Professor',
+        onChipAdd: function() {
+            M.updateTextFields();
+        }
+    });
+
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {
+        container: document.body
+    });
 });
 
